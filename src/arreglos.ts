67b84b9e4612c9d99236c9a1a0b84c8453ada90b
fhoras <104107218+fhoras@users.noreply.github.com>
2022-05-23 22:15:37 -0300

    Agregado de vector invertido Ejercicio de vector invertido

--- src/arreglos.ts
+++ src/arreglos.ts
@@ -1,13 +1,30 @@
 /* Construya un algoritmo que tenga un arreglo
 de dimensión deseada por el usuario y llénelo
 con los nombres que el usuario desee */
-function numeros_varios(cantidad: number) {
+let cant = document.getElementById("cantidad");
+
+function mostrarVectorInvertido(arreglo: number[]) {
+  let cantidad: number = arreglo.length;
+  let numeros_invertidos: number[] = [];
+  for (let i = cantidad - 1; 0 <= i; i--) {
+    numeros_invertidos[cantidad - 1 - i] = arreglo[i];
+    console.log(i);
+  }
+  console.log(numeros_invertidos);
+}
+
+function mostrar_numeros(cantidad: number) {
   let numero: number = 0;
   let numeros: number[] = [];
-  for (let i = 0; i <= cantidad; i++) {
+  for (let i = 0; i < cantidad; i++) {
     numero = prompt("Ingrese n°");
     numeros[i] = numero;
   }
   console.log(numeros);
+  mostrarVectorInvertido(numeros);
 }
-console.log(numeros_varios(5));
+
+btnDatos1.addEventListener("click", () => {
+  let cantidad: number = cant.value;
+  mostrar_numeros(cantidad);
+});
